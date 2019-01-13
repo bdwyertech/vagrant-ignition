6f6288cd563c5d7405b5f2aabbd4a56abf22d307
Brian Dwyer <bdwyer@IEEE.org>
2019-01-13 00:31:35 -0500

    Make the hook a bit more generic
    Signed-off-by: Brian Dwyer <bdwyer@IEEE.org>

--- lib/vagrant-ignition/plugin.rb
+++ lib/vagrant-ignition/plugin.rb
@@ -4,22 +4,16 @@ module VagrantPlugins
   module Ignition
     class Plugin < Vagrant.plugin("2")
       name "vagrant-ignition"
-      description "Ignition support for the virtualbox vagrant provider"
+      description "Ignition support for the Virtualbox & VMware Vagrant providers"
 
       config "ignition" do
         require_relative "config"
         Config
       end
-      
+
       action_hook(:setup_ignition, :machine_action_up) do |hook|
         require_relative "action/setup_ignition"
-        hook.after VagrantPlugins::ProviderVirtualBox::Action::Import, Action::SetupIgnition
-        if defined?(HashiCorp::VagrantVMwareworkstation)
-          hook.after HashiCorp::VagrantVMwareworkstation::Action::Import, Action::SetupIgnition
-        end
-        if defined?(HashiCorp::VagrantVMwarefusion)
-          hook.after HashiCorp::VagrantVMwarefusion::Action::Import, Action::SetupIgnition
-        end
+        hook.before(Vagrant::Action::Builtin::Provision, Action::SetupIgnition)
       end
     end
   end
